7eacabb8e26cc0c51ed47ec383f178531e6da1d1
Silvia Libras <silvialibras@gmail.com>
2025-06-01 22:32:23 -0300

    Initial commit

---
+++ src/main/java/com/latita/ProdutosApiApplication.java
@@ -0,0 +1,14 @@
+package com.latita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutosApiApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutosApiApplication.class, args);
+	}
+
+}
